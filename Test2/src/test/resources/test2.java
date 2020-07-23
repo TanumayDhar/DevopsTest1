764cc6ce8ad7823658f9cebb1db13efb140076e0
Tanumay Dhar <tanna.dhar@gmail.com>
2020-07-23 17:09:54 +0530

    First commit

---
+++ Test2/src/test/resources/test2.java
@@ -0,0 +1,10 @@
+
+public class test2 {
+	
+	public static void main ()
+	{
+		System.out.println("Git integration with Jenkins");
+		System.out.println(" two Git integration with Jenkins");
+	}
+
+}
