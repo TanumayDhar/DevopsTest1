b6e01cc930957d264aa2572f2333ed8b4918a9a8
Tanumay Dhar <tanna.dhar@gmail.com>
2020-07-28 12:42:26 +0530

    First commit

---
+++ DevopsSelenium/src/test/java/com/devops/seleniumDemo/NewTest.java
@@ -0,0 +1,15 @@
+package com.devops.seleniumDemo;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+  
+	
+	@Test
+  public void f() {
+		
+		System.out.println("demo");
+  }
+  
+  
+}
