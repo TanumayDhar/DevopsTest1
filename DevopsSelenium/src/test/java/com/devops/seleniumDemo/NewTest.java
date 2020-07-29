d8861c2a18c262c0db629be65c5d11ef708042df
Tanumay Dhar <tanna.dhar@gmail.com>
2020-07-29 18:53:41 +0530

    maven configure

--- DevopsSelenium/src/test/java/com/devops/seleniumDemo/NewTest.java
+++ DevopsSelenium/src/test/java/com/devops/seleniumDemo/NewTest.java
@@ -1,14 +1,23 @@
 package com.devops.seleniumDemo;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 public class NewTest {
   
-	
+	WebDriver driver;
 	@Test
-  public void f() {
+	public void f() {
 		
-		System.out.println("demo");
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Personal\\softwares\\chromedriver_win32\\chromedriver.exe");
+		driver = new ChromeDriver();
+		Reporter.log("Browser is launched");
+		driver.manage().window().maximize();
+		Reporter.log("Browser is maximized");
+		driver.quit();
   }
   
   
