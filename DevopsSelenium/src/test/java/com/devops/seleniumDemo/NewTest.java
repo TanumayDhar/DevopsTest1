ccdfabfc5cb4a1e09ee55a79d0d3af858eb7b034
Tanumay Dhar <tanna.dhar@gmail.com>
2020-07-30 17:07:14 +0530

    new code added

--- DevopsSelenium/src/test/java/com/devops/seleniumDemo/NewTest.java
+++ DevopsSelenium/src/test/java/com/devops/seleniumDemo/NewTest.java
@@ -2,23 +2,46 @@ package com.devops.seleniumDemo;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 public class NewTest {
-  
+
+
 	WebDriver driver;
-	@Test
-	public void f() {
-		
-		
+	String URL = "https://www.google.com/";
+
+	@Test(priority=1)
+	public void LaunchBrowser()
+	{
+
 		System.setProperty("webdriver.chrome.driver", "C:\\Personal\\softwares\\chromedriver_win32\\chromedriver.exe");
 		driver = new ChromeDriver();
 		Reporter.log("Browser is launched");
 		driver.manage().window().maximize();
 		Reporter.log("Browser is maximized");
-		driver.quit();
-  }
-  
-  
+
+
+	}
+	@Test(priority=2)
+	public void Urlhit()
+	{
+
+
+		driver.get(URL);
+		Reporter.log("Url is hit");
+	}
+	@Test(priority=3)
+	public void CloseBrowser()
+	{
+		String expectedTitle = "Google";
+		String actualTitle = driver.getTitle();
+		Assert.assertEquals(expectedTitle, actualTitle);
+		Reporter.log(expectedTitle);
+
+		driver.close();
+		Reporter.log("Browser is closed");
+
+	}
 }
